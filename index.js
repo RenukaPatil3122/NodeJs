15c44bc818eba3ebfda17a268c98b439e09c8f30
Renuka Patil <renukapatil627@gmail.com>
2026-03-26 15:56:47 +0530

    module scope

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const addFn = require("./add");
-
-console.log("Hello from index.js");
-
-const sum2 = addFn(2, 3);
-const sum = addFn(1, 2);
-console.log(sum);
-console.log(sum2);
+require("./batman");
+require("./superman");
