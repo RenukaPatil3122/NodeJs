e43b8b3fbf3b03362f18e0365914e21b42714203
Renuka Patil <renukapatil627@gmail.com>
2026-03-26 12:27:54 +0530

    module exports

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-require("./add");
+const addFn = require("./add");
 
 console.log("Hello from index.js");
+
+const sum2 = addFn(2, 3);
+const sum = addFn(1, 2);
+console.log(sum);
+console.log(sum2);
