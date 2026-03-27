37e43684476cfb8502be0a0136e70a19ae49c036
Renuka Patil <renukapatil627@gmail.com>
2026-03-27 17:03:41 +0530

    fs module

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const buffer = new Buffer.from("renuka");
+const fs = require("node:fs");
 
-buffer.write("code");
-console.log(buffer.toString());
-console.log(buffer);
-console.log(buffer.toJSON());
+console.log("first");
+const fileContents = fs.readFileSync("./file.txt", "utf-8");
+console.log(fileContents);
+
+console.log("second");
+
+fs.readFile("./file.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
+
+console.log("third");
+
+fs.writeFileSync("./greet.txt", "hello world");
+
+fs.writeFile("./greet.txt", " hello renuka", { flag: "a" }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("file written");
+  }
+});
