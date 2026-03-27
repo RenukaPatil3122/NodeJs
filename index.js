9d974f13f40e8ecaeb56734b3368a79e5c125c13
Renuka Patil <renukapatil627@gmail.com>
2026-03-27 17:32:28 +0530

    streams

--- index.js
+++ index.js
@@ -1,47 +1,13 @@
-const fs = require("node:fs/promises");
+const fs = require("node:fs");
 
-async function readFile() {
-  try {
-    const data = await fs.readFile("file.txt", "utf-8");
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-}
+const readableStream = fs.createReadStream("./file.txt", {
+  encoding: "utf-8",
+  highWaterMark: 2,
+});
 
-readFile();
+const writeableStream = fs.createWriteStream("./file2.txt");
 
-console.log("first");
-fs.readFile("file.txt", "utf-8")
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
-
-console.log("second");
-
-// const fs = require("node:fs");
-
-// console.log("first");
-// const fileContents = fs.readFileSync("./file.txt", "utf-8");
-// console.log(fileContents);
-
-// console.log("second");
-
-// fs.readFile("./file.txt", "utf-8", (error, data) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(data);
-//   }
-// });
-
-// console.log("third");
-
-// fs.writeFileSync("./greet.txt", "hello world");
-
-// fs.writeFile("./greet.txt", " hello renuka", { flag: "a" }, (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("file written");
-//   }
-// });
+readableStream.on("data", (chunk) => {
+  console.log(chunk);
+  writeableStream.write(chunk);
+});
