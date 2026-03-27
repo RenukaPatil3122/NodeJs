6416fc351cad6315a739aedf4e4b644ad3598e0e
Renuka Patil <renukapatil627@gmail.com>
2026-03-27 15:21:30 +0530

    Streams and Buffers

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-const PizzaShop = require("./pizza-shop");
-const DrinkMachine = require("./drink-machine");
+const buffer = new Buffer.from("renuka");
 
-const pizzaShop = new PizzaShop();
-const drinkMachine = new DrinkMachine();
-
-pizzaShop.on("order", (size, topping) => {
-  console.log(`order received! baking a ${size} pizza with ${topping}`);
-  drinkMachine.serveDrink(size);
-});
-
-pizzaShop.order("large", "mushrooms");
-pizzaShop.displayOrderNumber();
-
-// const EventEmitter = require("node:events");
-
-// const emitter = new EventEmitter();
-
-// emitter.on("order-pizza", (size, topping) => {
-//   console.log(`order received! baking a ${size} pizza with ${topping}`);
-// });
-
-// emitter.on("order-pizza", (size) => {
-//   if (size === "large") {
-//     console.log("serving complimentary drink");
-//   }
-// });
-
-// console.log("do work before event occurs in the system");
-
-// emitter.emit("order-pizza", "large", "mushrooms");
+buffer.write("code");
+console.log(buffer.toString());
+console.log(buffer);
+console.log(buffer.toJSON());
