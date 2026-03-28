f05bb2ef50a4da16bbedb768103d0890adfbc03c
Renuka Patil <renukapatil627@gmail.com>
2026-03-28 08:11:50 +0530

    html template

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
+  const name = "Renuka";
   res.writeHead(200, { "Content-Type": "text/html" });
-  fs.createReadStream("./index.html").pipe(res);
-  //   const html = fs.readFileSync("./index.html", "utf-8");
-  //   res.end(html);
+  let html = fs.readFileSync("./index.html", "utf-8");
+  html = html.replace("{{name}}", name);
+  res.end(html);
 });
 
 server.listen(3000, () => {
