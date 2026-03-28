04b3c0b396d78e43ad1473981e4b19700dcac09c
Renuka Patil <renukapatil627@gmail.com>
2026-03-28 08:31:10 +0530

    http routing

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const name = "Renuka";
-  res.writeHead(200, { "Content-Type": "text/html" });
-  let html = fs.readFileSync("./index.html", "utf-8");
-  html = html.replace("{{name}}", name);
-  res.end(html);
+  //req.method GET POST PUT DELETE
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        fname: "Bruce",
+        lname: "wayne",
+      }),
+    );
+  } else {
+    res.writeHead(404);
+    res.end("page not found");
+  }
 });
 
 server.listen(3000, () => {
