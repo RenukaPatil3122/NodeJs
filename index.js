aa64e2c588994b9c9c1c2effa6f16eb2d75dc999
Renuka Patil <renukapatil627@gmail.com>
2026-03-28 18:18:01 +0530

    ex 4 for settimeout and setImmediate

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+setTimeout(() => console.log("ST 1"), 0);
 setImmediate(() => console.log("this is setImmediate 1"));
-setImmediate(() => {
-  console.log("this is setImmediate 2");
-  process.nextTick(() => console.log("this is process.nextTick 1"));
-  Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
-});
-setImmediate(() => console.log("this is setImmediate 3"));
+
+// setImmediate(() => console.log("this is setImmediate 1"));
+// setImmediate(() => {
+//   console.log("this is setImmediate 2");
+//   process.nextTick(() => console.log("this is process.nextTick 1"));
+//   Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// });
+// setImmediate(() => console.log("this is setImmediate 3"));
 
 // const fs = require("fs");
 
