46501c925f228d65409e6c6fe8e01b962c9313b2
Renuka Patil <renukapatil627@gmail.com>
2026-03-29 14:28:18 +0530

    cli options

--- my-custom-cli/index.js
+++ my-custom-cli/index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log("code pokedex");
+const yargs = require("yargs");
+const { argv } = yargs(process.argv);
+
+console.log(process.argv);
+
+const printFiveMoves = async (pokemonName) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+  );
+  const pokemon = await response.json();
+  const moves = pokemon.moves.map(({ move }) => move.name);
+  console.log(moves.slice(0, 5));
+};
+
+printFiveMoves(argv.pokemon);
