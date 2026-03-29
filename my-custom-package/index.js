b31559b97edfe53b292f08a7f1b3512b76e46d7e
Renuka Patil <renukapatil627@gmail.com>
2026-03-29 12:30:30 +0530

    using packages

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -1,5 +1,7 @@
-function greet(name) {
-  console.log(`Hello ${name}`);
-}
+const upperCase = require("upper-case");
 
+function greet(name) {
+  console.log(upperCase(`Hello ${name}`));
+}
+greet("ren");
 module.exports = greet;
