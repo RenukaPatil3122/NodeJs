ba0262c435f457ec86471afb04138f629a036ed1
Renuka Patil <renukapatil627@gmail.com>
2026-03-29 11:22:39 +0530

    close queue

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-setTimeout(() => console.log("ST 1"), 0);
+const fs = require("fs");
+
+const readableStream = fs.createReadStream(__filename);
+readableStream.close();
+
+readableStream.on("close", () => {
+  console.log("this is from readableStream close event callback");
+});
 setImmediate(() => console.log("this is setImmediate 1"));
-
-// setImmediate(() => console.log("this is setImmediate 1"));
-// setImmediate(() => {
-//   console.log("this is setImmediate 2");
-//   process.nextTick(() => console.log("this is process.nextTick 1"));
-//   Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
-// });
-// setImmediate(() => console.log("this is setImmediate 3"));
-
-// const fs = require("fs");
-
-// fs.readFile(__filename, () => {
-//   console.log("this is readFile 1");
-//   setImmediate(() => console.log("inner setImmediate 1"));
-//   process.nextTick(() => console.log("inner process.nextTick 1"));
-//   Promise.resolve().then(() => console.log("inner Promise.resolve 1"));
-// });
-
-// process.nextTick(() => console.log("this is process.nextTick 1"));
-// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
-// setTimeout(() => console.log("ST 1"), 0);
-
-// for (let i = 0; i < 2000000000; i++) {}
+setTimeout(() => console.log("this is setTimeout 1"), 0);
+Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+process.nextTick(() => console.log("this is process.nextTick 1"));
